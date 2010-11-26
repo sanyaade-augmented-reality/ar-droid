e1a3063bafde5f8b113248b88360e57439eedc50
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2010-11-26 00:07:29 +0000

    se corrige referencia a EventCalendar

--- AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/Event.groovy
+++ AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/Event.groovy
@@ -4,7 +4,7 @@ class Event {
 	String title
 	String description
 	GeoPoint geoPoint;
-	EventCalenar eventCalenar 
+	EventCalendar eventCalenar 
 	static embedded = ['geoPoint']/**Esto indicar que el GeoPoint es una composición*/
 	
 	/**Conocimiento bidireccional con Activity y con la propiedad belongTo cada vez que se
