302049900e621efedc7e55e9a5d3473b934dc6d1
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2010-11-30 02:04:39 +0000

    clases para probar recupero de entiddades del servidor

---
+++ AR-DROID/src/ar/droid/Entities.java
@@ -0,0 +1,17 @@
+package ar.droid;
+
+import java.util.List;
+
+public class Entities {
+	List<Entity> entities;
+
+	public List<Entity> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(List<Entity> entities) {
+		this.entities = entities;
+	}
+
+	
+}
