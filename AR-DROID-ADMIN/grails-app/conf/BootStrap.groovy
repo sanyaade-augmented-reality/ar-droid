15fccd6444ec39fa0144bcfbd0cb6ac03d4ad3ae
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-02-16 02:13:05 +0000

    Se quita clase de ejemplo Book

--- AR-DROID-ADMIN/grails-app/conf/BootStrap.groovy
+++ AR-DROID-ADMIN/grails-app/conf/BootStrap.groovy
@@ -1,10 +1,8 @@
-import ar.droid.admin.Book;
-
 class BootStrap {
 
      def init = { servletContext ->
         // Create some test data
-        new Book(author:"Stephen King",title:"The Shining").save()
+        // new Book(author:"Stephen King",title:"The Shining").save()
         //new Book(author:"James Patterson",title:"Along Came a Spider").save()
     }
     def destroy = {
