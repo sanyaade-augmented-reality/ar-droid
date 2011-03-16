b1eb391b309eaa3645db70401eb1452ddc4ebbbb
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-03-16 05:06:32 +0000

    se quita url y photo de actividad, va a evento.

--- AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/Activity.groovy
+++ AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/Activity.groovy
@@ -1,26 +1,23 @@
 package ar.droid.admin
 
-import org.hibernate.annotations.Columns;
-import org.hibernate.annotations.Type;
 
 class Activity {
 	
-	static hasMany = [events:Event]
-	
 	String name	
 	String description
-	String url
-	byte[] photo
-	
+	GeoPoint geoPoint	
 	TypeActivity typeActivity
+	Entity entity
 	
-	/*Conocimiento bidireccional con Entidad y con la propiedad belongTo cada vez que se
-	 * elimine una Entidad se eliminan sus actividades*/
-	static belongsTo = [entity:Entity]
-	static mapping = {
+	static belongsTo = [entity: Entity]
+	
+	static mapping = { 
 		description type: 'text'
 	}
-	static constraints = {
-		url(url:true)
-    }
+	static constraints = { 
+		name(blank: false)
+	}
+	
+	/**Esto indicar que el GeoPoint es una composición*/
+	static embedded = ['geoPoint'] 
 }
