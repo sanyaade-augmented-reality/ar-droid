50fdbb4d8cb4ab7a80b1b80819d55928522609fa
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-03-16 05:05:26 +0000

    icono en tipo de entidad.

--- AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/TypeEntity.groovy
+++ AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/TypeEntity.groovy
@@ -3,11 +3,19 @@ package ar.droid.admin
 class TypeEntity {
 
 	String description
+	byte[] icon
+	
     static constraints = {
+		description(blank: false)
+		icon(nullable:true)
     }
 	
 	@Override
 	public String toString() {
 		return description
+	}
+	
+	static mapping = {
+		icon sqlType: 'blob'
 	}
 }
