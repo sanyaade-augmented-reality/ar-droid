c5bf826f7511745ead28337ca50bb7848b83df1b
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-03-30 03:14:47 +0000

    nuevos test generados automaticamente

---
+++ AR-DROID-ADMIN/test/unit/ar/droid/admin/EventTypeTests.groovy
@@ -0,0 +1,17 @@
+package ar.droid.admin
+
+import grails.test.*
+
+class EventTypeTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
