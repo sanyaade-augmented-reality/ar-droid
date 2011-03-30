2a926d74b3646a21dd0691fc9b213ffa422fdade
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-03-30 03:13:36 +0000

    servicios para entidad y evento

---
+++ AR-DROID-ADMIN/grails-app/services/ar/droid/admin/EntityService.groovy
@@ -0,0 +1,64 @@
+package ar.droid.admin
+
+import ar.droid.admin.reader.ReaderNews
+import ar.droid.admin.reader.ReaderActivity
+
+class EntityService {
+	static transactional = true
+	
+	def Entity saveEntity(params) {
+		def entityInstance = new Entity()
+		entityInstance.properties = params
+		entityInstance.readerActivity = ReaderActivity.fromString(params.readerActivity_select)
+		entityInstance.readerNews = ReaderNews.fromString(params.readerNews_select)
+		
+		// crear composiciones
+		entityInstance.readerActivity.properties = params.readerActivity
+		entityInstance.readerNews.properties = params.readerNews
+		
+		entityInstance.geoPoint = new GeoPoint()
+		
+		// validar posición
+		if(params.latitude == null || ''.equals(params.latitude) || params.longitude == null || ''.equals(params.longitude)){
+			entityInstance.errors.rejectValue('geoPoint', 'You must select a GeoPoint')
+		}
+		else {
+			// armar geopoint
+			entityInstance.geoPoint.latitude = new BigDecimal(params.latitude)
+			entityInstance.geoPoint.longitude = new BigDecimal(params.longitude)
+			
+			entityInstance.save(flush: true)
+		}
+		return entityInstance;
+	}
+	
+	def Entity updateEntity(params) {
+		def entityInstance = Entity.get(params.id)
+		if (entityInstance){
+			if (params.version) {
+				def version = params.version.toLong()
+				if (entityInstance.version > version) {
+					entityInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'entity.label', default: 'Entity')] as Object[], "Another user has updated this Entity while you were editing")
+					return entityInstance
+				}
+			}
+			entityInstance.properties = params
+			
+			// verificar si cambiar o mantener imagen
+			def photo = entityInstance.photo;
+			if(params.get("photo").size == 0){
+				entityInstance.photo = photo;
+			}
+			
+			entityInstance.readerActivity = ReaderActivity.fromString(params.readerActivity_select)
+			entityInstance.readerNews = ReaderNews.fromString(params.readerNews_select)
+		
+			// crear composiciones
+			entityInstance.readerActivity.properties = params.readerActivity
+			entityInstance.readerNews.properties = params.readerNews
+			
+			entityInstance.save(flush: true)
+		}
+		return entityInstance
+	}
+}
---
+++ AR-DROID-ADMIN/grails-app/services/ar/droid/admin/EventService.groovy
@@ -0,0 +1,55 @@
+package ar.droid.admin
+
+import ar.droid.admin.calendar.EventCalendar
+
+class EventService {	
+	static transactional = true
+	
+	def Event saveEvent(params) {
+		def eventInstance = new Event()
+		eventInstance.properties = params
+		eventInstance.eventCalendar = EventCalendar.fromString(params.readerActivity_select)
+		
+		// crear composiciones
+		eventInstance.eventCalendar.properties = params.eventCalendar
+		
+		eventInstance.geoPoint = new GeoPoint()
+		
+		// validar posición
+		if(params.latitude == null || ''.equals(params.latitude) || params.longitude == null || ''.equals(params.longitude)){
+			eventInstance.errors.rejectValue('geoPoint', 'You must select a GeoPoint')
+		}
+		else {
+			// armar geopoint
+			eventInstance.geoPoint.latitude = new BigDecimal(params.latitude)
+			eventInstance.geoPoint.longitude = new BigDecimal(params.longitude)
+			
+			eventInstance.save(flush: true)
+		}
+		return eventInstance;
+	}
+	
+	def Event updateEvent(params) {
+		def eventInstance = Event.get(params.id)
+		if (eventInstance){
+			if (params.version) {
+				def version = params.version.toLong()
+				if (eventInstance.version > version) {
+					eventInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'entity.label', default: 'Entity')] as Object[], "Another user has updated this Entity while you were editing")
+					return entityInstance
+				}
+			}
+			
+			// verificar si cambiar o mantener imagen
+			def photo = eventInstance.photo;
+			eventInstance.properties = params
+			eventInstance.readerActivity = ReaderActivity.fromString(params.readerActivity_select)
+			entityInstance.readerNews = ReaderNews.fromString(params.readerNews_select)
+			if(params.get("photo").size == 0){
+				entityInstance.photo = photo;
+			}
+			eventInstance.save(flush: true)
+		}
+		return entityInstance
+	}
+}
