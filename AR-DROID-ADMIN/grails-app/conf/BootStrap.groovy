af76b632e1b741c4a8312a36eb2e47d1c68a926b
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-03-30 03:14:23 +0000

    instanciación de elementos comunes

--- AR-DROID-ADMIN/grails-app/conf/BootStrap.groovy
+++ AR-DROID-ADMIN/grails-app/conf/BootStrap.groovy
@@ -1,11 +1,15 @@
+import ar.droid.admin.reader.*
+import ar.droid.admin.calendar.*
+
 class BootStrap {
-
-     def init = { servletContext ->
-        // Create some test data
-        // new Book(author:"Stephen King",title:"The Shining").save()
-        //new Book(author:"James Patterson",title:"Along Came a Spider").save()
-    }
-    def destroy = {
-    }
-
+	
+	def init = { servletContext ->
+		servletContext.lsReaderNews = [new ReaderNewsNone(), new ReaderNewsRSS(), new ReaderNewsWeb(), new ReaderNewsFacebook()]
+		servletContext.lsReaderActivities = [new ReaderActivityNone(), new ReaderActivityFacebook(), new ReaderActivityApi()]
+		servletContext.lsEventCalendars = [new Unique(), new Daily(), new Weekly(), new Monthly()]
+		servletContext.mpDayOfTheWeek = ["0": "Sunday", "1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday"]
+	}
+	
+	def destroy = {
+	}
 }
