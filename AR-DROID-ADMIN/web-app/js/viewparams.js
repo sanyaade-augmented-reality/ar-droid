6753abea317c37a077758151f0f3e0f1d17ac626
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-04-02 22:07:38 +0000

    se define la funcion showDataForQuestion para mostrar los datos a ingresar en una pregunta segun el tipo

--- AR-DROID-ADMIN/web-app/js/viewparams.js
+++ AR-DROID-ADMIN/web-app/js/viewparams.js
@@ -4,19 +4,19 @@ function viewParams(input){
 		case 'readerActivity_select':
 			if(value == 'class ar.droid.admin.reader.ReaderActivityNone'){
 				input.value = '';
-				$('readerActivity_parameter').style.display = 'none';
+				$('readerActivity_parameter').hide();
 			}
 			else
-				$('readerActivity_parameter').style.display = '';
+				$('readerActivity_parameter').show();
 		break;
 		
 		case 'readerNews_select':
 			if(value == 'class ar.droid.admin.reader.ReaderNewsNone'){
 				input.value = '';
-				$('readerNews_parameter').style.display = 'none';
+				$('readerNews_parameter').hide();
 			}
 			else
-				$('readerNews_parameter').style.display = '';
+				$('readerNews_parameter').show();
 		break;
 		
 		case 'eventCalendar_select':
@@ -28,4 +28,38 @@ function viewParams(input){
 				$('dayOfTheMonth').style.display = '';
 		break;
 	}
+}
+
+function showDataForQuestion(input){
+	var value = input.value;
+	switch(value){
+		case '3':
+			$('trMultipleChoiceType').show();
+			$('trNumericType').hide();
+			break;
+		case '2':
+			$('trMultipleChoiceType').hide();
+			$('trNumericType').show();
+			deleteOptions();
+			break;
+		case '1':
+			$('trMultipleChoiceType').hide();
+			$('trNumericType').hide();
+			deleteOptions();
+			break;
+	}
+}
+
+function createOption(button){
+	var tr = new Element('tr', { 'class': 'prop','name':'trChoice'}).update("<tr><td><label for='choice'>Opcion</label></td><td><input type='text' name='choice' id='choice'></td><td>&nbsp;</td><td>&nbsp;</td></tr>");
+	Element.insert($('tableQuestion'), tr);
+}
+
+function deleteOptions(){
+	var trs = $('tableQuestion').getElementsBySelector('[name="trChoice"]');
+	//alert($A(trs).length);
+	trs.each(function(row){
+		Element.remove(row);
+	});
+	
 }
