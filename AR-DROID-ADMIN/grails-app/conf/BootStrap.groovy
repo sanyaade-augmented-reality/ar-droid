2a127fc03eb064601deb1b4036b1bdac4d59f06c
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-04-13 00:33:42 +0000

    se incorpora creacion de tipo de evento y template por defecto

--- AR-DROID-ADMIN/grails-app/conf/BootStrap.groovy
+++ AR-DROID-ADMIN/grails-app/conf/BootStrap.groovy
@@ -1,5 +1,8 @@
 import ar.droid.admin.reader.*
 import ar.droid.admin.calendar.*
+import ar.droid.admin.*;
+import ar.droid.admin.survey.*;
+import ar.droid.admin.survey.question.*;;
 
 class BootStrap {
 	
@@ -8,6 +11,31 @@ class BootStrap {
 		servletContext.lsReaderActivities = [new ReaderActivityNone(), new ReaderActivityFacebook(), new ReaderActivityApi()]
 		servletContext.lsEventCalendars = [new Unique(), new Daily(), new Weekly(), new Monthly()]
 		servletContext.mpDayOfTheWeek = ["0": "Sunday", "1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday"]
+		
+		//tendria que crear si no existe el tipo de evento 1-ninguno
+		// template te gusta si/no
+		
+		def typeEvent = TypeEvent.get(1)
+		if (typeEvent == null){
+			typeEvent = new TypeEvent(name:"Sin Clasificación")
+			typeEvent.save(flush:true)
+		}
+		
+		def template = SurveyTemplate.get(1)
+		if (template == null){
+			template = new SurveyTemplate(description:"Estándar AR-DROID")
+			def question = new MultipleChoiceQuestion(question:"Te gusta",surveyTemplate:template,maxOptions:1)
+			def ch1 = new Choice(description:"Si",multipleChoiceQuestion:question)
+			def ch2 = new Choice(description:"No",multipleChoiceQuestion:question)
+			template.questions = []
+			template.questions.add(question)
+			question.options = []
+			question.options.add(ch1)
+			question.options.add(ch2)
+			template.save(flush:true)
+			log.error(template.errors)
+		}
+		
 	}
 	
 	def destroy = {
