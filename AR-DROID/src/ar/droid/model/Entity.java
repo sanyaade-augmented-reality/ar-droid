58001974aa27725c92bf2e7d07800df7e5cca692
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-05-03 05:04:27 +0000

    se incorpora el geopoint

---
+++ AR-DROID/src/ar/droid/model/Entity.java
@@ -0,0 +1,36 @@
+package ar.droid.model;
+
+import com.google.android.maps.GeoPoint;
+
+public class Entity {
+	String name;
+	String description;
+	String url;
+	GeoPoint geoPoint;
+	
+	public GeoPoint getGeoPoint() {
+		return geoPoint;
+	}
+	public void setGeoPoint(GeoPoint geoPoint) {
+		this.geoPoint = geoPoint;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
