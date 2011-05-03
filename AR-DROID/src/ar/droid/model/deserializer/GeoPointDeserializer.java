09ae7d5179c614bcfa0d4f1cf32d3131ab588544
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-05-03 05:06:09 +0000

    para desearilizar objetos de tipo geopoin. Se utiliza en el mapeo de json a un objeto del modelo

---
+++ AR-DROID/src/ar/droid/model/deserializer/GeoPointDeserializer.java
@@ -0,0 +1,26 @@
+package ar.droid.model.deserializer;
+
+import java.lang.reflect.Type;
+
+import android.util.Log;
+
+import com.google.android.maps.GeoPoint;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+public class GeoPointDeserializer implements JsonDeserializer<GeoPoint> {
+
+	@Override
+	public GeoPoint deserialize(JsonElement json, Type type,JsonDeserializationContext context) throws JsonParseException {
+		Log.i("latitude",json.getAsJsonObject().get("latitude").getAsString());
+		Log.i("longitude",json.getAsJsonObject().get("longitude").getAsString());
+		
+		 double lat = json.getAsJsonObject().get("latitude").getAsDouble();
+		 double lng = json.getAsJsonObject().get("longitude").getAsDouble();
+		 GeoPoint p = new GeoPoint((int) (lat * 1E6), (int) (lng * 1E6)); 
+		return p;
+	}
+
+}
