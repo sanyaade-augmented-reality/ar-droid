3b36f58f94d68c45a4713761351be93dc720b215
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-05-28 22:13:23 +0000

    se incorpora mapeo ReaderNews y se cambia el objeto geopoint al nuevo subclasificado

--- AR-DROID/src/ar/droid/model/Entity.java
+++ AR-DROID/src/ar/droid/model/Entity.java
@@ -1,14 +1,32 @@
 package ar.droid.model;
 
+import ar.droid.admin.reader.ReaderNews;
+
 import ar.droid.location.GeoPoint;
 
 public class Entity {
-	String name;
-	String description;
-	String url;
-	GeoPoint geoPoint;
-	String photoUrl;
-	TypeEntity typeEntity;
+	private Long id;
+	private String name;
+	private String description;
+	private String url;
+	private GeoPoint geoPoint;
+	private String photoUrl;
+	private TypeEntity typeEntity;
+	
+	private ReaderNews readerNews;
+	
+	public ReaderNews getReaderNews() {
+		return readerNews;
+	}
+	public void setReaderNews(ReaderNews readerNews) {
+		this.readerNews = readerNews;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
 	public TypeEntity getTypeEntity() {
 		return typeEntity;
@@ -46,5 +64,29 @@ public class Entity {
 	public void setUrl(String url) {
 		this.url = url;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entity other = (Entity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 	
 }
