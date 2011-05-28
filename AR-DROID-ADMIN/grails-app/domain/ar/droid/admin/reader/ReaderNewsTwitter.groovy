dd13308d9a2baf9837c8eabb53f47153687fe38a
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-05-28 21:37:53 +0000

    read news para twitter

---
+++ AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/reader/ReaderNewsTwitter.groovy
@@ -0,0 +1,17 @@
+package ar.droid.admin.reader
+
+class ReaderNewsTwitter extends ReaderNews {
+	
+	static mapping = {
+		tablePerHierarchy false
+	}
+	
+    static constraints = {
+		parameter(blank:false)
+    }
+	
+	@Override
+	public String toString(){
+		return "Twitter";
+	}
+}
