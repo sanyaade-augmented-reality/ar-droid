1164d5221e3b042d42b5bd5be2f65696918857f3
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-05-29 15:16:44 +0000

    cache de imagenes en imagehelper

--- AR-DROID/src/ar/droid/resources/ImageHelper.java
+++ AR-DROID/src/ar/droid/resources/ImageHelper.java
@@ -1,6 +1,7 @@
 package ar.droid.resources;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 import android.graphics.drawable.Drawable;
@@ -9,17 +10,21 @@ import ar.droid.connection.ConnectionUtil;
 
 
 public abstract class ImageHelper implements IImageHelper {
-	private Map<String, Drawable> images;
+	private static Map<String, Drawable> _IMAGES = null;
 	
+	public ImageHelper(){
+		if(_IMAGES == null)
+			_IMAGES = new HashMap<String, Drawable>();
+	}
 	protected Drawable loadImage(String url) {
-		if(this.images.containsKey(url))
-			return this.images.get(url);
+		if(_IMAGES.containsKey(url))
+			return _IMAGES.get(url);
 					
 		String urlServer = ARDROIDProperties.getInstance().getProperty("ar.droid.server");
 		InputStream instream = ConnectionUtil.getInputStream(urlServer+url);
 		if (instream != null){
 			Drawable drawable = Drawable.createFromStream(instream, "offers task");
-			this.images.put(url, drawable);
+			_IMAGES.put(url, drawable);
 			return drawable;
 		}
 		return null;
