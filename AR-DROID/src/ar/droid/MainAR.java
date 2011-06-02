c05be637c832a7d9b11ff019dee39cb3f1d4f83f
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-06-02 04:18:38 +0000

    implementación ontouch para RA

--- AR-DROID/src/ar/droid/MainAR.java
+++ AR-DROID/src/ar/droid/MainAR.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.logging.Logger;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.location.Location;
@@ -12,6 +13,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup.LayoutParams;
 import ar.droid.ar.activity.AugmentedView;
 import ar.droid.ar.activity.SensorsActivity;
@@ -20,170 +24,201 @@ import ar.droid.ar.common.ARData;
 import ar.droid.ar.common.DataSource;
 import ar.droid.ar.view.Marker;
 import ar.droid.config.ARDROIDProperties;
+import ar.droid.model.Entity;
+import ar.droid.view.EntityTabWidget;
 
-public class MainAR extends SensorsActivity {
+public class MainAR extends SensorsActivity implements OnTouchListener {
 	static String TAG = MainAR.class.getName();
 	private static final Logger logger = Logger.getLogger(MainAR.class.getSimpleName());
 	private static final String locale = Locale.getDefault().getLanguage();
-	
+
 	private static DataSource source = null;
-	
-	private static CameraSurface camScreen = null;    
-    private static AugmentedView augmentedView = null;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        logger.info("onCreate()");
+	private static CameraSurface camScreen = null;
+	private static AugmentedView augmentedView = null;
 
-        camScreen = new CameraSurface(this);
-        setContentView(camScreen);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		logger.info("onCreate()");
 
-        augmentedView = new AugmentedView(this);
-        LayoutParams augLayout = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        addContentView(augmentedView, augLayout);
-        
-        updateDataOnZoom();
-             
-        // crear el source
-        if (source == null) {
-        	source = new DataSource();
-        }
-    }
-    
-    @Override
-    public void onDestroy() {
-    	super.onDestroy();
-    	logger.info("onDestroy()");
-    }
-    
-    @Override
-    public void onStart() {
-    	super.onStart();
-    	logger.info("onStart()");
-    	
-    	Location last = ARData.getCurrentLocation();
-        if (last!=null) updateData(last.getLatitude(),last.getLongitude(),last.getAltitude());
-    }
-    
-    @Override
-    public void onStop() {
-    	super.onStop();
-    	logger.info("onStop()");
-    }
-    
-    @Override
-    public void onSensorChanged(SensorEvent evt) {
-        super.onSensorChanged(evt);
+		camScreen = new CameraSurface(this);
+		setContentView(camScreen);
 
-        if (    evt.sensor.getType() == Sensor.TYPE_ACCELEROMETER || 
-                evt.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD
-        ) {
-            augmentedView.postInvalidate();
-        }
-    }
-    
-    private static float calcZoomLevel(){
-        int zoomLevel = ARDROIDProperties.getInstance().getIntProperty("ar.droid.ar.zoomlevel");
-        float myout = 5;
-    
-        if (zoomLevel <= 26) {
-            myout = zoomLevel / 25f;
-        } else if (25 < zoomLevel && zoomLevel < 50) {
-            myout = (1 + (zoomLevel - 25)) * 0.38f;
-        } else if (25== zoomLevel) {
-            myout = 1;
-        } else if (50== zoomLevel) {
-            myout = 10;
-        } else if (50 < zoomLevel && zoomLevel < 75) {
-            myout = (10 + (zoomLevel - 50)) * 0.83f;
-        } else {
-            myout = (30 + (zoomLevel - 75) * 2f);
-        }
+		augmentedView = new AugmentedView(this);
+		LayoutParams augLayout = new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+		addContentView(augmentedView, augLayout);
 
-        return myout;
-    }
+		updateDataOnZoom();
 
-    protected void updateDataOnZoom() {
-        float zoomLevel = calcZoomLevel();
-        ARData.setRadius(zoomLevel);
-        ARData.setZoomLevel(String.valueOf(zoomLevel));
-        
-        int defZoomLevel = ARDROIDProperties.getInstance().getIntProperty("ar.droid.ar.zoomlevel");
-        ARData.setZoomProgress(defZoomLevel);
-    };
-    
-    @Override
-    public void onLocationChanged(Location location) {
-        super.onLocationChanged(location);
-        updateData(location.getLatitude(),location.getLongitude(),location.getAltitude());
-    }
-    
-    private static Thread thread = null;
-    private void updateData(final double lat, final double lon, final double alt) {
-    	if (thread!=null && thread.isAlive()) {
-    		logger.info("Not updating since in the process");
-    		return;
-    	}
-    	
-    	thread = new Thread(
-    		new Runnable(){
-				@Override
-				public void run() {
-					logger.info("empieza");
-					download(source, lat, lon, alt);
-					logger.info("termina");
-				}
-			}
-    	);
-    	thread.start();
-    }
-    
-    private static boolean download(DataSource source, double lat, double lon, double alt) {
-		if (source == null) return false;
-		
-		    	
-    	List<Marker> markers = source.getEntities();
-    	if (markers==null) return false;
-    	
-    	logger.info(source.getClass().getSimpleName()+" size="+markers.size());
-    	
-    	ARData.addMarkers(markers);
-    	return true;
-    }
-    
-    @Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.menu_ar, menu);
-	    return true;
+		// crear el source
+		if (source == null) {
+			source = new DataSource();
+		}
 	}
-	
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		logger.info("onDestroy()");
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		logger.info("onStart()");
+
+		Location last = ARData.getCurrentLocation();
+		if (last != null)
+			updateData(last.getLatitude(), last.getLongitude(), last.getAltitude());
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		logger.info("onStop()");
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent evt) {
+		super.onSensorChanged(evt);
+
+		if (evt.sensor.getType() == Sensor.TYPE_ACCELEROMETER
+				|| evt.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+			augmentedView.postInvalidate();
+		}
+	}
+
+	private static float calcZoomLevel() {
+		int zoomLevel = ARDROIDProperties.getInstance().getIntProperty("ar.droid.ar.zoomlevel");
+		float myout = 5;
+
+		if (zoomLevel <= 26) {
+			myout = zoomLevel / 25f;
+		} else if (25 < zoomLevel && zoomLevel < 50) {
+			myout = (1 + (zoomLevel - 25)) * 0.38f;
+		} else if (25 == zoomLevel) {
+			myout = 1;
+		} else if (50 == zoomLevel) {
+			myout = 10;
+		} else if (50 < zoomLevel && zoomLevel < 75) {
+			myout = (10 + (zoomLevel - 50)) * 0.83f;
+		} else {
+			myout = (30 + (zoomLevel - 75) * 2f);
+		}
+
+		return myout;
+	}
+
+	protected void updateDataOnZoom() {
+		float zoomLevel = calcZoomLevel();
+		ARData.setRadius(zoomLevel);
+		ARData.setZoomLevel(String.valueOf(zoomLevel));
+
+		int defZoomLevel = ARDROIDProperties.getInstance().getIntProperty("ar.droid.ar.zoomlevel");
+		ARData.setZoomProgress(defZoomLevel);
+	};
+
+	@Override
+	public void onLocationChanged(Location location) {
+		super.onLocationChanged(location);
+		updateData(location.getLatitude(), location.getLongitude(), location.getAltitude());
+	}
+
+	private static Thread thread = null;
+
+	private void updateData(final double lat, final double lon, final double alt) {
+		if (thread != null && thread.isAlive()) {
+			logger.info("Not updating since in the process");
+			return;
+		}
+
+		thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				logger.info("empieza");
+				download(source, lat, lon, alt);
+				logger.info("termina");
+			}
+		});
+		thread.start();
+	}
+
+	private static boolean download(DataSource source, double lat, double lon, double alt) {
+		if (source == null)
+			return false;
+
+		List<Marker> markers = source.getEntities();
+		if (markers == null)
+			return false;
+
+		logger.info(source.getClass().getSimpleName() + " size=" + markers.size());
+
+		ARData.addMarkers(markers);
+		return true;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_ar, menu);
+		return true;
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-		    case R.id.menu_mapa:
-		    	
-		    	// volver al mapa
-		    	this.finish();
-		        return true;
-		    case R.id.menu_find:
-		        return true;		        
-		    case R.id.menu_show:
-		        return true;
-		    default:
-		        return super.onOptionsItemSelected(item);
-		    case R.id.menu_close:
-		    	this.exit();
-		        return true;
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.menu_mapa:
+
+			// volver al mapa
+			this.finish();
+			return true;
+		case R.id.menu_find:
+			return true;
+		case R.id.menu_show:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		case R.id.menu_close:
+			this.exit();
+			return true;
+		}
 	}
-	
-	private void exit(){
+
+	private void exit() {
 		Log.d(TAG, "Salir");
-    	int pid = android.os.Process.myPid(); 
-    	android.os.Process.killProcess(pid);
+		int pid = android.os.Process.myPid();
+		android.os.Process.killProcess(pid);
+	}
+
+	@Override
+	public boolean onTouch(View view, MotionEvent me) {
+		return false;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent me) {
+		if (me.getAction() != MotionEvent.ACTION_UP)
+			return false;
+		
+		Entity entity = this.getMarkerAt(me.getX(), me.getY());
+		if(entity == null)
+			return false;
+		Intent i = new Intent(this.getApplicationContext(), EntityTabWidget.class);
+		i.putExtra("idEntity", entity.getId());
+		this.startActivity(i);
+		return true;
+	}
+
+	private Entity getMarkerAt(float x, float y) {
+		for (int i = 0 ; i < ARData.getMarkerCount(); i++) {
+			Marker marker = ARData.getMarker(i);
+			if(marker.clickedMe(x, y))
+				return marker.getEntity();
+		}
+		return null;
 	}
 }
