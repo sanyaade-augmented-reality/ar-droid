24509c2c7e76347d54374e0c676cbd69210ca508
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-06-04 16:15:52 +0000

    pantalla completa de info de entidad

--- AR-DROID/src/ar/droid/view/EntityTabWidget.java
+++ AR-DROID/src/ar/droid/view/EntityTabWidget.java
@@ -3,6 +3,8 @@ package ar.droid.view;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TabHost;
 import ar.droid.R;
 
@@ -15,12 +17,17 @@ public class EntityTabWidget extends TabActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// pantalla completa
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		//se setea el layout para tabs
 		setContentView(R.layout.tab_host);
 		
 		//TabHost del la actividad
 		TabHost tabHost = getTabHost();
-		
 		
 		//se crea inten para lanzar la actividad correspondiente al tab main
 		Intent intent =  new Intent().setClass(this, EntityView.class);
