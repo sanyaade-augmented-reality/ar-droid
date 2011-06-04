45a191e0a7add58c25a0e799db2e16abf830a17e
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-06-04 16:09:53 +0000

    nueva pantalla de bienvenida

--- AR-DROID/src/ar/droid/SplashScreen.java
+++ AR-DROID/src/ar/droid/SplashScreen.java
@@ -1,52 +1,79 @@
 package ar.droid;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+import ar.droid.config.ARDROIDProperties;
+import ar.droid.model.Resource;
 
 public class SplashScreen extends Activity {
-    protected boolean _active = true;
-    protected int _splashTime = 2000;
-    
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-                
-        // crear hilo para mostrar el splash screen
-        Thread splashTread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    int waited = 0;
-                    while(_active && (waited < _splashTime)) {
-                        sleep(100);
-                        if(_active) {
-                            waited += 100;
-                        }
-                    }
-                } catch(InterruptedException e) {
-                    // no hacer nada
-                } finally {
-                    Intent myIntent = new Intent(SplashScreen.this, MainMap.class);
-                    SplashScreen.this.startActivity(myIntent);
-                    finish();
-                    stop();
-                }
-            }
-        };
-        splashTread.start();
-    }
-    
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            _active = false;
-        }
-        return true;
-    }
+	static String TAG = SplashScreen.class.getName();
+
+	protected boolean _waitLoaded = true;
+	protected int _splashTime = 3000;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// pantalla completa
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.splash);
+
+		// crear hilo para mostrar el splash screen
+		Thread splashTread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					int waited = 0;
+					while ((waited < _splashTime) || _waitLoaded) {
+						sleep(100);
+						waited += 100;
+					}
+				} catch (InterruptedException e) {
+					// no hacer nada
+				} finally {
+					Intent myIntent = new Intent(SplashScreen.this, MainMap.class);
+					SplashScreen.this.startActivity(myIntent);
+					finish();
+					stop();
+				}
+			}
+		};
+		splashTread.start();
+		try {
+			ARDROIDProperties.createProperties(this.getApplicationContext());
+			Resource.getInstance().getEntities();
+			_waitLoaded = false;
+		} catch (Exception e) {
+			Log.e(TAG, "ERROR", e);
+			showError();
+		}
+	}
+
+	private void showError() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("No se puede conectar con el servidor!").setCancelable(false)
+				.setPositiveButton("Cerrar", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						int pid = android.os.Process.myPid();
+						android.os.Process.killProcess(pid);
+					}
+				});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
 }
