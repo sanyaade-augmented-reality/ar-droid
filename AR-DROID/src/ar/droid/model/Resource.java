7d4de7d8eb114498c1a8f5ec1eea281b49f07251
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-06-04 16:15:08 +0000

    intentar obtener entidades sino existen

--- AR-DROID/src/ar/droid/model/Resource.java
+++ AR-DROID/src/ar/droid/model/Resource.java
@@ -3,32 +3,33 @@ package ar.droid.model;
 import java.util.List;
 
 public class Resource {
-	
+
 	private static Resource resource;
-	
-	private Resource(){
+
+	private Resource() {
 	}
-	
+
 	public List<Entity> entities = null;
-	
-	public  List<Entity> getEntities(){
-		if (entities ==null)
-			entities = 	 ResourceHelperFactory.createResourceHelper().getEntities();
+
+	public List<Entity> getEntities() {
+		if (entities == null)
+			entities = ResourceHelperFactory.createResourceHelper().getEntities();
 		return entities;
 	}
-	
-	public Entity getEntity(Entity entity){
-		if (entities != null){
-			int index = entities.indexOf(entity);
-			if (index != -1){
-				return entities.get(index);
-			}
-		}	
+
+	public Entity getEntity(Entity entity) {
+		if (entities == null)
+			this.getEntities();
+		
+		int index = entities.indexOf(entity);
+		if (index != -1) {
+			return entities.get(index);
+		}
 		return null;
 	}
-	
-	public static Resource getInstance(){
-		if (resource == null){
+
+	public static Resource getInstance() {
+		if (resource == null) {
 			resource = new Resource();
 		}
 		return resource;
