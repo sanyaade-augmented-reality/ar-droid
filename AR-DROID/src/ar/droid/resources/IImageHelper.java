f7405e18ac15649a5e29b22804f0908100bf2e94
colman.marisa <colman.marisa@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-06-07 05:03:55 +0000

    se cambia recupero de imagenes

--- AR-DROID/src/ar/droid/resources/IImageHelper.java
+++ AR-DROID/src/ar/droid/resources/IImageHelper.java
@@ -1,8 +1,14 @@
 package ar.droid.resources;
 
 import android.graphics.drawable.Drawable;
+import ar.droid.model.Entity;
+import ar.droid.model.Event;
+import ar.droid.model.TypeEntity;
 
 public interface IImageHelper {
 	public Drawable getImage(String Url);
+	public Drawable getImageEvent(Event event);
+	public Drawable getImageEntity(Entity entity);
+	public Drawable getIconTypeEntity(TypeEntity typeEntity);
 	
 }
--- AR-DROID/src/ar/droid/resources/ImageHelperHTTP.java
+++ AR-DROID/src/ar/droid/resources/ImageHelperHTTP.java
@@ -1,11 +1,30 @@
 package ar.droid.resources;
 
 import android.graphics.drawable.Drawable;
+import ar.droid.config.Request;
+import ar.droid.model.Entity;
+import ar.droid.model.Event;
+import ar.droid.model.TypeEntity;
 
 public class ImageHelperHTTP extends ImageHelper {
 
 	public Drawable getImage(String Url) {
 		return  loadImage(Url);
+	}
+
+	@Override
+	public Drawable getImageEvent(Event event) {
+		return loadImage(Request.GET_IMAGE_EVENT + "/" + event.getId());
+	}
+
+	@Override
+	public Drawable getImageEntity(Entity entity) {
+		return loadImage(Request.GET_IMAGE_ENTITY + "/" + entity.getId());
+	}
+
+	@Override
+	public Drawable getIconTypeEntity(TypeEntity typeEntity) {
+		return loadImage(Request.GET_ICON_TYPE_ENTITY + "/" + typeEntity.getId());
 	}
 
 }
