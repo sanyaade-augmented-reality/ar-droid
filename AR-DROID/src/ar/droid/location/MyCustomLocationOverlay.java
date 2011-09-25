a3590f7b87572dd2fcd9801d891e4df8d2a7c36f
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-09-25 18:22:48 +0000

    orientacion correcta de posición

--- AR-DROID/src/ar/droid/location/MyCustomLocationOverlay.java
+++ AR-DROID/src/ar/droid/location/MyCustomLocationOverlay.java
@@ -1,4 +1,5 @@
 package ar.droid.location;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -13,47 +14,45 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 
 public class MyCustomLocationOverlay extends MyLocationOverlay {
-    private Context mContext;
-    private float   mOrientation;
+	private Context mContext;
+	private float mOrientation;
 
-    public MyCustomLocationOverlay(Context context, MapView mapView) {
-        super(context, mapView);
-        mContext = context;
-    }
+	public MyCustomLocationOverlay(Context context, MapView mapView) {
+		super(context, mapView);
+		mContext = context;
+	}
 
-    @Override 
-    protected void drawMyLocation(Canvas canvas, MapView mapView, Location lastFix, GeoPoint myLocation, long when) {
-        // translate the GeoPoint to screen pixels
-        Point screenPts = mapView.getProjection().toPixels(myLocation, null);
+	@Override
+	protected void drawMyLocation(Canvas canvas, MapView mapView,
+			Location lastFix, GeoPoint myLocation, long when) {
+		// translate the GeoPoint to screen pixels
+		Point screenPts = mapView.getProjection().toPixels(myLocation, null);
 
-        // create a rotated copy of the marker
-        Bitmap arrowBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.arrow);
-        Matrix matrix = new Matrix();
-        matrix.postRotate(this.getOrientation());
-        Bitmap rotatedBmp = Bitmap.createBitmap(
-            arrowBitmap, 
-            0, 0, 
-            arrowBitmap.getWidth(), 
-            arrowBitmap.getHeight(), 
-            matrix, 
-            true
-        );
-        // add the rotated marker to the canvas
-        canvas.drawBitmap(
-            rotatedBmp, 
-            screenPts.x - (rotatedBmp.getWidth()  / 2), 
-            screenPts.y - (rotatedBmp.getHeight() / 2), 
-            null
-        );
-        mapView.postInvalidate();
-    }
-    
-    @Override
-    public synchronized void onLocationChanged(Location location) {
-    	super.onLocationChanged(location);
-    }
-    
-    public void setOrientation(float newOrientation) {
-         mOrientation = newOrientation;
-    }
+		// create a rotated copy of the marker
+		Bitmap arrowBitmap = BitmapFactory.decodeResource(
+				mContext.getResources(), R.drawable.arrow);
+		Matrix matrix = new Matrix();
+		matrix.postRotate(this.getOrientation());
+		Bitmap rotatedBmp = Bitmap.createBitmap(arrowBitmap, 0, 0,
+				arrowBitmap.getWidth(), arrowBitmap.getHeight(), matrix, true);
+		// add the rotated marker to the canvas
+		canvas.drawBitmap(rotatedBmp,
+				screenPts.x - (rotatedBmp.getWidth() / 2), screenPts.y
+						- (rotatedBmp.getHeight() / 2), null);
+		mapView.postInvalidate();
+	}
+
+	@Override
+	public synchronized void onLocationChanged(Location location) {
+		super.onLocationChanged(location);
+	}
+
+	public void setOrientation(float newOrientation) {
+		mOrientation = newOrientation;
+	}
+
+	@Override
+	public float getOrientation() {
+		return ((super.getOrientation() - 360) * -1);
+	}
 }
