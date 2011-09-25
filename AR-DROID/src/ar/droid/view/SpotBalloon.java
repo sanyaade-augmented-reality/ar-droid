cd03f4918491e3389aecce27f5b40d5f0d9942bc
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-09-25 21:20:57 +0000

    tomar tamaño de configuracion para iconos de eventos

--- AR-DROID/src/ar/droid/view/SpotBalloon.java
+++ AR-DROID/src/ar/droid/view/SpotBalloon.java
@@ -3,52 +3,50 @@ package ar.droid.view;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
+import ar.droid.config.ARDroidPreferences;
 
-public class SpotBalloon extends Drawable {
+public class SpotBalloon extends BitmapDrawable {
 
 	private Paint spotBalloonPaint = new Paint();
-    private static final Paint blackTextPaint = new Paint();
-    private static final int HEIGHT = 20;
-    private static final int WIDTH = 100;
-    //private String name;
-    
-    public SpotBalloon(int alpha, int r, int g, int b) {
-         spotBalloonPaint.setARGB(alpha, r, g, b);
-         spotBalloonPaint.setAntiAlias(true);
-         blackTextPaint.setARGB(255, 0, 0, 0);
-         blackTextPaint.setAntiAlias(true);
-    }
-   
-    public SpotBalloon(int color) {
-    	spotBalloonPaint.setColor(color);
-    	spotBalloonPaint.setAntiAlias(true);
-    	blackTextPaint.setARGB(255, 0, 0, 0);
-    	blackTextPaint.setAntiAlias(true);    	
-    }
+	private static final Paint blackTextPaint = new Paint();
+	private static final int HEIGHT = 20;
+	private static final int WIDTH = 100;
 
-    @Override
-    public void draw(Canvas canvas) {          
-         canvas.setViewport(WIDTH, HEIGHT);
-        canvas.drawCircle(0, 0, 11, blackTextPaint);
-         canvas.drawCircle(0, 0, 10, spotBalloonPaint);
-         
-    }
+	// private String name;
 
-    @Override
-    public int getOpacity() {
-         return 0;
-    }
+	public SpotBalloon(int alpha, int r, int g, int b) {
+		spotBalloonPaint.setARGB(alpha, r, g, b);
+		spotBalloonPaint.setAntiAlias(true);
+		blackTextPaint.setARGB(255, 0, 0, 0);
+		blackTextPaint.setAntiAlias(true);
+	}
 
-    @Override
-    public void setAlpha(int alpha) {
-    }
+	public SpotBalloon(int color) {
+		spotBalloonPaint.setColor(color);
+		spotBalloonPaint.setAntiAlias(true);
+		blackTextPaint.setARGB(255, 0, 0, 0);
+		blackTextPaint.setAntiAlias(true);
+	}
 
-    @Override
-    public void setColorFilter(ColorFilter cf) {
-    }
-    
-    /*public void setName(String name){
-         this.name = name;
-    }*/
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.setViewport(WIDTH, HEIGHT);
+		canvas.drawCircle(0, 0, 7 * ARDroidPreferences.getInt("iconSizePref", 2), blackTextPaint);
+		canvas.drawCircle(0, 0, 7 * ARDroidPreferences.getInt("iconSizePref", 2), spotBalloonPaint);
+
+	}
+
+	@Override
+	public int getOpacity() {
+		return 0;
+	}
+
+	@Override
+	public void setAlpha(int alpha) {
+	}
+
+	@Override
+	public void setColorFilter(ColorFilter cf) {
+	}
 }
