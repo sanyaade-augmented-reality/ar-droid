9029723d420a18437b7fed739ac2f81f7dedf9d7
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-10-02 16:38:30 +0000

    Nuevo tipo de calendario: día laboral

---
+++ AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/calendar/WorkingDaily.groovy
@@ -0,0 +1,32 @@
+package ar.droid.admin.calendar
+
+import java.util.Calendar
+
+class WorkingDaily extends EventCalendar{
+
+	static constraints = {
+	}
+
+	@Override
+	public String toString() {
+		return "WorkingDaily";
+	}
+
+	def isToday = {
+		Date hoy = new Date()
+		if(hoy.compareTo(this.startDate) >= 0)
+			if(hoy.getAt(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && hoy.getAt(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY)
+			return true
+		else
+			return false
+	}
+
+	def isWeekle = {
+		if(hoy.compareTo(this.startDate) >= 0)
+			return true
+		else
+			return false
+	}
+
+	def isMonthly = { return this.isWeekle() }
+}
