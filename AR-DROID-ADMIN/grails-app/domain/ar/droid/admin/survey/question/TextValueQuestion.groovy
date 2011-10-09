475c4eb1b002c3efe46329942bcf4a157c65c4aa
gabrielnegri@gmail.com <gabrielnegri@gmail.com@088dcc3c-b66c-7266-ef30-686d1a321a2b>
2011-10-09 19:30:31 +0000

    Mejora en el armado de comentarios

--- AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/survey/question/TextValueQuestion.groovy
+++ AR-DROID-ADMIN/grails-app/domain/ar/droid/admin/survey/question/TextValueQuestion.groovy
@@ -1,5 +1,8 @@
 package ar.droid.admin.survey.question
 
+import java.sql.Date;
+import java.sql.Timestamp
+
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import ar.droid.admin.survey.response.SurveyResponse;
@@ -20,11 +23,19 @@ class TextValueQuestion extends Question {
 	
 	def getSummary (responses){
 		def commentarios = []
+		
+		def dueDateFromTimestamp = {
+			assert it instanceof java.sql.Timestamp
+			['DD/MM/YY': [String.format('%td', it), String.format('%tm', it), String.format('%tY', it)].join("-") ]
+		}
+		
 		for (SurveyResponse sr : responses) {
 			TextValueResponse tvr = sr.first()
-			commentarios.add(">> " + tvr.getComment() + ". \n\r")
+			Calendar date = new GregorianCalendar()
+			date.setTimeInMillis(tvr.surveyResponse.fecha.getTime())
+			commentarios.add(date.getAt(Calendar.DAY_OF_MONTH) + "/" + (date.getAt(Calendar.MONTH)+1) + "/" +  date.getAt(Calendar.YEAR).toString().substring(2) + ": " + tvr.getComment() + "\n\r")
 		}
-		return ["rating":-1,"description":responses.size() + " commentarios","comments":commentarios]
+		return ["rating":-1,"description": responses.size() + " comentarios","comments":commentarios]
 	}
 	
 }
